a9f135b07ad4b74ce19dfa8f0e3fec0d077c1865
Igor.Koloski <koloski_igor@hotmail.com>
2021-06-07 17:03:00 +0200

    Added authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,15 @@
 class PostsController < ApplicationController
 
-    before_action :authenticate_user!, only: [:index]
+    before_action :authenticate_user!, except: [:index]
 
     def new
         @post = Post.new()
     end
+
+    def index
+        @post = Post.all
+        @user = User.all
+      end
 
     def create
         @post = Post.new(post_params)
